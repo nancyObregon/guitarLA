0db26de835a93d42d7fa2e80a73d01b39e613412
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2023-05-04 16:27:01 -0600

    Routing con Link

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Link from "next/link"
 
 export default function Home({}) {
 
   return (
     <>
       <h1>Hola</h1>
+      <Link href={"/nosotros"}>Nosotros</Link>
     </>
   )
 }
---
+++ pages/nosotros.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Nosotros() {
+    return (
+        <div>
+            <Link href="/">Ir a inicio</Link>
+
+        </div>
+    )
+}
