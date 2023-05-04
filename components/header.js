b48b59668967e8fafe1443660384220fdff6e7e0
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2023-05-04 17:12:30 -0600

    Se termina header

--- components/header.js
+++ components/header.js
@@ -1,25 +1,41 @@
 import Image from 'next/future/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 export default function Header() {
+
+    const router = useRouter()
+
     return (
         <header className={styles.header}>
             <div className={`contenedor ${styles.barra}`}>
-                <Image src={'/img/logo.svg'}  width={300} height={40} alt='Logotipo' />
+                <Link href={'/'}>
+                    <a>
+                        <Image src={'/img/logo.svg'}  width={300} height={40} alt='Logotipo' />
+                    </a>
+                </Link>
 
                 <nav className={styles.navegacion}>
-                    <Link href={'/'}>
-                        Inicio
+                    <Link href={'/'} >
+                        <a className={ router.pathname === '/' ? styles.active : ''}>
+                            Inicio
+                        </a>
                     </Link>
                     <Link href={'/nosotros'}>
-                        Nosotros
+                        <a className={ router.pathname === '/nosotros' ? styles.active : ''}>
+                            Nosotros
+                        </a>
                     </Link>
                     <Link href={'/blog'}>
-                        Blog
+                        <a className={ router.pathname === '/blog' ? styles.active : ''}>
+                            Blog
+                        </a>
                     </Link>
                     <Link href={'/tienda'}>
-                        Tienda
+                        <a className={ router.pathname === '/tienda' ? styles.active : ''}>
+                            Tienda
+                        </a>
                     </Link>
                 </nav>
             </div>
