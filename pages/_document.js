065dc0bf69516bf1e54b27894cbec13253fc6735
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2023-05-04 16:22:52 -0600

    Se agrega normalize

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,11 @@ export default function Document(){
     return (
         <Html>
             <Head>
+                <meta name="description" content="GuitarLA - Venta de Guitarras y blog de musica"/>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap" rel="stylesheet"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'} />
+                <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"/>
             </Head>
             <body>
                 <Main/>
