468a14dd24877a0a7356abb6d22777be0472930c
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2023-05-04 17:23:23 -0600

    Estilos a footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,18 @@
+import Link from "next/link"
+import styles from '../styles/footer.module.css'
+
 export default function Footer() {
     return (
-        <div>footer</div>
+        <footer className={styles.footer}>
+            <div className={`contenedor ${styles.contenido}`}>
+                <nav className={styles.navegacion}>
+                    <Link href={"/"}>Inicio</Link>
+                    <Link href={"/nosotros"}>Nosotros</Link>
+                    <Link href={"/blog"}>Blog</Link>
+                    <Link href={"/tienda"}>Tienda</Link>
+                </nav>
+                <p className={styles.copy}>Todos los derechos reservados {new Date().getFullYear()}</p>
+            </div>
+        </footer>
     )
 }
