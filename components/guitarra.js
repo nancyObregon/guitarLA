9f6c43113eac8dd8f8405f59467ad2b40f4e41ea
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2023-05-05 17:25:06 -0600

    Se agregan estilos

--- components/guitarra.js
+++ components/guitarra.js
@@ -1,25 +1,27 @@
-import Image from "next/image"
+import Image from "next/future/image"
 import React from 'react'
 import Link from "next/link"
+import styles from '../styles/guitarras.module.css'
+
 
 export default function Guitarra({guitarra}) {
 
   const { descripcion, imagen, nombre, precio, url } = guitarra
   return (
-    <div>
+    <div className={styles.guitarra}>
       <Image 
         src={imagen.data.attributes.formats.medium.url} 
         alt={`Imagen guitarra ${nombre}`}  
-        height={600} 
-        width={400}
+        width={600}
+        height={400} 
       />
-      <div>
+      <div className={styles.contenido}>
         <h3>{nombre}</h3>
-        <p>{descripcion}</p>
-        <p>${precio}</p>
+        <p className={styles.descripcion}>{descripcion}</p>
+        <p className={styles.precio}>${precio}</p>
         <Link href={`/guitarras/${url}`}>
-          <a>
-
+          <a className={styles.enlace}>
+            Ver Producto
           </a>
         </Link>
       </div>
