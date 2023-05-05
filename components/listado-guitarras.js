3b6fcde40692d9e1bd579acef610141aa0693a02
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2023-05-05 16:55:08 -0600

    Se agrega fetch utilizando getStaticProps

---
+++ components/listado-guitarras.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ListadoGuitarras() {
+  return (
+    <div>listado-guitarras</div>
+  )
+}
--- pages/tienda.js
+++ pages/tienda.js
@@ -1,12 +1,28 @@
 import Layout from "../components/layout"
 
-export default function Tienda() {
+export default function Tienda({guitarras}) {
+    console.log(guitarras)
     return (
         <Layout
             title={"Tienda virtual"}
             description="Tienda Virtual, guitarLA"
         >
-            <h1>Tienda Virtual</h1>
+            <main className="contenedor">
+                <h1 className="heading">
+                    Nuestra Colección
+                </h1>
+            </main>
         </Layout>
     )
+}
+
+export async function getStaticProps(){
+    const respuesta = await fetch(`http://127.0.0.1:1337/api/guitarras?populate=imagen`)
+    const {data: guitarras} = await respuesta.json()
+
+    return {
+        props: {
+            guitarras
+        }
+    }
 }
